759d392e01497c48fc75f596fe17ff6b5afedd73
Vedika Pangavhane <vedikapangavhane2007>
2026-08-24 23:29:03 +0530

    Remove Launch_App.bat and Launch_App.sh from App/Source_Code/Additional_Files

--- App/Source_Code/Additional_Files/Launch_App.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: Launch_App.bat - Opens Google Drive download link for Launch_App script
-echo Opening Launch_App.bat download link...
-start "" "https://drive.google.com/file/d/15TcDZuNGD3Cu81OjIz_G94IX02aJqkjK/view?usp=drive_link"
